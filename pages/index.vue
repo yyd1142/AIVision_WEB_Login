3229471cdecfefcf4b0b464ed481e5f28e33da60
YYD <yyd-yyd@hotmail.com>
2018-09-06 18:32:45 +0800

    提交代码

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page-wrap">
+
+  </div>
+</template>
+
+<style lang="less">
+.page-wrap
+{
+  width: 100%;
+  display: flex;
+  display: -webkit-flex;
+  flex: 1;
+
+}
+</style>
